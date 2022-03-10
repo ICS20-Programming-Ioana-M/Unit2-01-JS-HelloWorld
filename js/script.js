88d3e25f08360ba44ae1e03569390c939e36ad12
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-03-10 16:01:27 +0000

    made entirety of website

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Created by: Ioana
+// Created on: Mar 10th 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  
+  // this function displays 'Hello-world'
+  document.getElementById('hello-world').innerHTML = "Never gonna give you up, never gonna let you down, never gonna run around and desert you, never gonna make you cry, never gonna say goodbye, never gonna tell a lie and hurt you"
+}
